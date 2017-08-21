97808958e44bd0365ec868b384823fb9f40e7bfc
Steven Huang <szteven3@gmail.com>
2017-08-21 17:03:40 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JenkinsRailsApp::Application.config.secret_token = '836f49dcc04d99d9b22c13ed2aa0573e5cbd4e399ce08efc999ddc3195cdfb5d891bf41cf21b3c73a842f3cb7bcac5694479d0672598c8198807a3977b0f1cfa'
